3fefc65235a887c6e5ac733747aff50ef35e42eb
jayant <jayanttripathy1985@gmail.com>
2022-09-25 21:14:21 +0530

    Add project files.

---
+++ JWTLoginAuthenticationAuthorization/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using JWTLoginAuthenticationAuthorization.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace JWTLoginAuthenticationAuthorization.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        //For admin Only
+        [HttpGet]
+        [Route("getAdminUser")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult AdminEndPoint()
+        {
+            var currentUser = GetCurrentUser();
+            return Ok($"Hi, wWelcome you are a {currentUser.Role}");
+        }
+        //For EndUser Only
+        [HttpGet]
+        [Route("getEndUser")]
+        [Authorize(Roles = "EndUser")]
+        public IActionResult UserEndPoint()
+        {
+            var currentUser = GetCurrentUser();
+            return Ok($"Hi, wWelcome you are a {currentUser.Role}");
+        }
+        private UserModel GetCurrentUser()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                return new UserModel
+                {
+                    Username = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value,
+                    Role = userClaims.FirstOrDefault(x => x.Type == ClaimTypes.Role)?.Value
+                };
+            }
+            return null;
+
+        }
+    }
+}
---
+++ JWTLoginAuthenticationAuthorization/Models/UserConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace JWTLoginAuthenticationAuthorization.Models
+{
+    // We are not taking data from data base so we get data from constant
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new()
+            {
+                    new UserModel(){ Username="jayant",Password="J@321$",Role="Admin"},
+                    new UserModel(){ Username="dev",Password="J@321$",Role="EndUser"},
+            };
+    }
+}
